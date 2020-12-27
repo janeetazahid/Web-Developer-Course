49d1f865da3c9f525fa3a1a16b30bf688a38dc3a
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2020-12-26 21:15:03 -0500

    update

--- JavaScript/intro.js
+++ JavaScript/intro.js
@@ -4,5 +4,7 @@ console.log(year+=5)
 year--
 console.log(year)
 
-const birthday=05; //constant values are defined like this 
-console.log(birthday) 
+
+
+let username="Janeeta"
+console.log(username)
