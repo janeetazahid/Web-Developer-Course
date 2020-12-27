384b14a6750f73ee1a87b90a675c0fd7a5b91768
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2020-12-27 18:54:43 -0500

    update

--- JavaScript/Intro/intro.js
+++ JavaScript/Intro/intro.js
@@ -1,8 +1,12 @@
 console.log("Hello World");
 let total=2+9;
-
+let random=Math.random()
 //if statements 
-let rating=3;
-if (rating===3){
-    console.log("You are a superstar");
+if (random>0.5){
+    console.log("your number is greater than 0.5")
+    console.log(random)
+}
+if (random<=0.5){
+    console.log("your number is less than  or equals 0.5")
+    console.log(random)
 }
