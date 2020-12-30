5460d3c6ef8f985f737ab66c29af207fa6447249
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2020-12-29 21:06:25 -0500

    update

---
+++ JavaScript/objects.js
@@ -0,0 +1,10 @@
+//JS objects 
+//obejcts are a collection of properties, with key-value pairs 
+
+
+const person={
+                firstname: 'Mick',
+                lastName: 'Jagger'
+            }           
+
+console.log(person)
