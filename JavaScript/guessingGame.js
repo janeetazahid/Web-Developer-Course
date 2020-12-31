f81fee5e9f824313918d369175d1a0ab72b140c9
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2020-12-30 21:00:05 -0500

    update

--- JavaScript/guessingGame.js
+++ JavaScript/guessingGame.js
@@ -17,16 +17,29 @@ console.log(targetNum)
 let guess=parseInt(prompt("Enter your first guess"));
 let attempts=1; //number of guesses 
 
-while (guess!==targetNum)
+while (parseInt(guess)!==targetNum)
     {
+        if (guess==='q') 
+            {
+                break;
+            }
+            
         attempts++;
         if (guess>targetNum)
             {
-                guess=parseInt(prompt("Too high! Enter a new guess!"))
+                guess=prompt("Too high! Enter a new guess!")
             }
         else
             {
-                guess=parseInt(prompt("Too low! Enter a new guess!"))
+                guess=prompt("Too low! Enter a new guess!")
             }
     }
-console.log(`You got it! It took you ${attempts} guesses`)
+if (guess==='q')
+    {
+        console.log("Quiting...")
+    }
+else
+{
+    console.log(`You got it! It took you ${attempts} guesses`)
+}
+
