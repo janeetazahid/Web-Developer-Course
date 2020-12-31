00115f7068ee9c9bfb4f2c816a7b9643b3788f6e
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2020-12-30 21:46:54 -0500

    update

--- JavaScript/todo.js
+++ JavaScript/todo.js
@@ -1,15 +1,25 @@
 
 let todoList=[]//array for todo
+userInput=prompt("What would you like to do?");
 
-
-while (True)
+while (userInput!=="quit")
     {
+        
         userInput=prompt("What would you like to do?");
         if (userInput==="new")
             {
                 todo=prompt("Enter new todo");
-                
-                console.log(`${todo} added to list`)
+                todoList.unshift(todo); //add to array
+                console.log(`${todo} added to list`);
 
             }
+        if (userInput==="list")
+        {
+            console.log("********")
+            for(i=0;i<=todoList.length;i++)
+                {
+                    console.log(i+" : "+todoList[i])
+                }
+                console.log("********")
+        }
     }
