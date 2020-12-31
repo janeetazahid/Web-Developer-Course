a6219d1fe92d9ad2b1ba87f93a40ba09796148b0
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2020-12-30 22:04:53 -0500

    update

--- JavaScript/todo.js
+++ JavaScript/todo.js
@@ -1,6 +1,6 @@
 
 let todoList=['wash car','laundary']//array for todo
-userInput=prompt("What would you like to do?");
+userInput=prompt("What would you like to do?"); //get user input
 
 while (userInput!=="quit")
     {
@@ -8,7 +8,7 @@ while (userInput!=="quit")
         
         if (userInput==="new")
             {
-                todo=prompt("Enter new todo");
+                todo=prompt("Enter new todo"); //get new todo to add 
                 todoList.push(todo); //add to array
                 console.log(`${todo} added to list`);
 
@@ -18,20 +18,20 @@ while (userInput!=="quit")
             console.log("********")
             for(i=0;i<todoList.length;i++)
                 {
-                    console.log(i+": "+todoList[i])
+                    console.log(i+": "+todoList[i]) //print list 
                 }
                 console.log("********")
         }
         else if (userInput==="delete")
         {
             let indexDel=parseInt(prompt("which list entry would you like to delete?"));
-            if(!Number.isNaN(indexDel))
+            if(!Number.isNaN(indexDel)) //make sure its a valid number
             {
-                const del=todoList.splice(indexDel,1);
+                const del=todoList.splice(indexDel,1); //delete specified index
                 console.log(`Deleted ${del}`);
             }
             else{
-                console.log("Unknown Index");
+                console.log("Unknown Index"); //else its a unknown index
             }
             
         }
