d49693307b235a24cdc09fd886eae95b3a3c78dd
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2020-12-30 21:36:37 -0500

    update

---
+++ JavaScript/todo.js
@@ -0,0 +1,15 @@
+
+let todoList=[]//array for todo
+
+
+while (True)
+    {
+        userInput=prompt("What would you like to do?");
+        if (userInput==="new")
+            {
+                todo=prompt("Enter new todo");
+                
+                console.log(`${todo} added to list`)
+
+            }
+    }
