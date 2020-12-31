9b9536af784a5257afac611f6ab600d504834454
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2020-12-30 21:10:18 -0500

    update

--- JavaScript/forOfLoop.js
+++ JavaScript/forOfLoop.js
@@ -4,3 +4,9 @@ const subreddits=['cringe','books','chickens','funny','pics','soccer']
 for(let sub of subreddits){
     console.log(sub)
 }
+
+
+for (let char of "hello world")
+    {
+        console.log(char)
+    }
