bfa840cf496c1af1b177d75b039a5438532e1f26
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2020-12-30 20:38:15 -0500

    update

--- JavaScript/loops.js
+++ JavaScript/loops.js
@@ -5,6 +5,13 @@ for (i=0;i<=10;i++){
 }
 
 // Print out "Da ba dee da ba daa" 6 times, using a for loop
-for (i=0;i<=5;i++){
-    console.log("Da ba dee da ba daa")
+// for (i=0;i<=5;i++){
+//     console.log("Da ba dee da ba daa")
+// }
+const SECRET="red";
+let guess=prompt("enter the secrate code...");
+while(guess!==SECRET){
+    guess=prompt("enter the secrate code...");
 }
+console.log("Congrats you got it")
+
