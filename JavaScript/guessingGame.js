88226488dbf6da0966a7e51f458aba9247768680
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2020-12-30 20:48:20 -0500

    update

--- JavaScript/guessingGame.js
+++ JavaScript/guessingGame.js
@@ -1,4 +1,9 @@
 
-let maxNum=prompt("Enter the max number"); 
+let maxNum=parseInt(prompt("Enter the max number")); 
+
+while (!maxNum)
+    {
+        maxNum=parseInt(prompt("Enter a valid number, Please")); 
+    }
 const targetNum=Math.floor(Math.random()*maxNum)+1;
 console.log(targetNum)
