b592f9bac62b1d62951b0fdaa982a7363a1c8405
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2021-01-01 18:37:25 -0500

    update

---
+++ JavaScript/arrowFunctions.js
@@ -0,0 +1,34 @@
+const nums=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+
+const movies=[
+    {
+        title:'Amadues',
+        score:99
+    },
+    {
+        title:'Stand by Me',
+        score:85
+    },
+    {
+        title:'Parasite',
+        score:95
+    },
+    {
+        title:'Alien',
+        score:90
+    }
+]
+
+
+const addArr=(arr)=>{
+    let i=0;
+    let sum=0;
+    for (i;i<arr.length;i++)
+    {
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+console.log(addArr(nums))
