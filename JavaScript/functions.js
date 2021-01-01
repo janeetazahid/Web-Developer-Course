713f9e87efcd2a5551800025a94b435a26fb4927
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2020-12-31 21:43:38 -0500

    update

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -109,3 +109,15 @@ callTwice(rollDie)
 
 
 
+
+function makeBetweenFunc(min,max)
+{
+    return function(num){
+        return num>=min && num<=max;
+    }
+}
+
+const isChild=makeBetweenFunc(0,18)
+
+
+console.log(isChild(15))
