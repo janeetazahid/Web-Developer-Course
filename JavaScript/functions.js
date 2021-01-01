fb1fb0f78ac176d4f9d06a51d23acea59d478db7
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2020-12-31 20:51:40 -0500

    update

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -6,3 +6,83 @@ function singSong()
     }
 
 singSong()
+
+//multiplication function
+function multiply(num1,num2)
+{
+    return num1*num2
+}
+
+
+// function that checks tempreture and determies wether the weather 
+//good enough to wear shorts (less than 75F) or not 
+function isShortsWeather(temp)
+{
+    if(temp>=75)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+
+
+//function that returns last element in array
+function lastElement(arr)
+{
+    if (arr.length==0)
+    {
+        return null;
+    }
+    return (arr[arr.length-1]);
+}
+
+
+// functio that takes in a string and returns same string with the 
+// first letter captilized 
+function capitalize(str)
+{
+    let str2='';
+    let i=0;
+    for (i;i<str.length;i++)
+    {
+        if (i===0)
+        {
+            str2+=str[i].toUpperCase();
+        }
+        else 
+        {
+            str2+=str[i];
+        }
+    }
+    return str2;
+}
+
+//function that returns sum of elements in array
+function sumArray(arr)
+{
+    let sum=0;
+    let i=0;
+    for (i;i<arr.length;i++)
+    {
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+
+//takes in a number between 1-7 and returns a string of the day of 
+// the week that corresponds to the number 
+function returnDay(num)
+{
+    if (num<1 || num>7)
+    {
+        return null;
+    }
+    let arr_day=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    return arr_day[num-1];
+}
+
+
