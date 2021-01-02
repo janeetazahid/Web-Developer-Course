f4ad004b276b9205a9bd139d7a3fb689f8e9fe4b
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2021-01-01 22:22:59 -0500

    upate

--- JavaScript/restParams.js
+++ JavaScript/restParams.js
@@ -4,3 +4,13 @@
 
 
  console.log(sumAll(1,2,3))
+
+
+ function raceResults(gold,silver,...everyoneElse)
+ {
+     console.log(`Gold Medal goes to: ${gold}`)
+     console.log(`Silver Medal goes to: ${silver}`)
+     console.log(`and thanks to everyone else: ${everyoneElse}`)
+ }
+
+ raceResults('Tina','Tod','Bob','Tom','Stacey','Bobby')
