b93e64763a3a3f3abd25c6df5967add8a11fffb3
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2021-01-01 22:02:13 -0500

    update

--- JavaScript/spread.js
+++ JavaScript/spread.js
@@ -2,3 +2,11 @@ const nums=[2,3,2,8]
 
 //spreads array into seprate arguments 
 console.log(Math.max(...nums))
+
+
+const cats=['Blue','Scout','Rocket']
+const dogs=['Rusty','Wyatt']
+
+
+const allPets=[...cats, ...dogs]
+console.log(allPets)
