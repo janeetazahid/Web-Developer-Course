2cdf8ca92af95a8842aad57a86f61a184fdf7aa9
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2021-01-02 20:14:20 -0500

    update

--- DOM/app.js
+++ DOM/app.js
@@ -6,3 +6,6 @@ for (let img of allImages)
 {
     img.src='https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80'
 }
+
+
+document.querySelector('#banner') //will find first of this type
