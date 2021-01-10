5e87c78b12951a6e140abcdce72cb57a3df4b781
Janeeta Zahid <janeetazahid@gmail.com>
2021-01-09 21:48:59 -0500

    update

--- DOM/pokemon.js
+++ DOM/pokemon.js
@@ -1,4 +1,14 @@
 
 const container=document.querySelector('#container');
-const newImg=document.createElement('img');
-newImg.src=''
+
+//base address
+let baseAddr='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+for(let i=0;i<10;i++)
+{
+    //create a new image 
+    const newImg=document.createElement('img');
+    //create source for image 
+    newImg.src=`${baseAddr}${i}.png`
+    //append image to container
+    container.appendChild(newImg)
+}
