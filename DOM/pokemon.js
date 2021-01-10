97c9c43c4596726c8e045a8cadcffca2ccfa78b3
Janeeta Zahid <janeetazahid@gmail.com>
2021-01-09 21:50:16 -0500

    update loop

--- DOM/pokemon.js
+++ DOM/pokemon.js
@@ -3,7 +3,7 @@ const container=document.querySelector('#container');
 
 //base address
 let baseAddr='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
-for(let i=0;i<10;i++)
+for(let i=1;i<151;i++)
 {
     //create a new image 
     const newImg=document.createElement('img');
