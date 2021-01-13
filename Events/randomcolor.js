23532ae74d573ec26f2be8962bceed9dd8973a65
Janeeta Zahid <janeetazahid@gmail.com>
2021-01-12 20:54:58 -0500

    update

--- Events/randomcolor.js
+++ Events/randomcolor.js
@@ -1,12 +1,19 @@
 const button=document.querySelector('button') //select button 
-
+const h1=document.querySelector('h1')
 
 //add event
 button.addEventListener('click',function(){
 
+    const newColor=makeRandomColor()
+    document.body.style.backgroundColor=newColor; //set that as background color
+    h1.innerText=newColor;
+})
+
+
+const makeRandomColor=()=>{
     const r=Math.floor(Math.random()*255); //randomly generate RGB color
     const g=Math.floor(Math.random()*255);
     const b=Math.floor(Math.random()*255);
-    const newColor=`rgb(${r},${g},${b})`;
-    document.body.style.backgroundColor=newColor; //set that as background color
-})
+    return newColor=`rgb(${r},${g},${b})`;
+
+}
