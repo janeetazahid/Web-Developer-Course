e126007fb6e428846a551a859e3abdb3aa9a353d
Janeeta Zahid <janeetazahid@gmail.com>
2021-01-12 21:13:02 -0500

    changing font color

--- Events/randomcolor.js
+++ Events/randomcolor.js
@@ -1,12 +1,15 @@
 const button=document.querySelector('button') //select button 
 const h1=document.querySelector('h1')
 
+
 //add event
 button.addEventListener('click',function(){
 
     const newColor=makeRandomColor()
+    
     document.body.style.backgroundColor=newColor; //set that as background color
     h1.innerText=newColor;
+    
 })
 
 
@@ -14,6 +17,11 @@ const makeRandomColor=()=>{
     const r=Math.floor(Math.random()*255); //randomly generate RGB color
     const g=Math.floor(Math.random()*255);
     const b=Math.floor(Math.random()*255);
+    if ((r+g+b)<500){
+        console.log('yes')
+        h1.style.color='white'
+    }
+
     return newColor=`rgb(${r},${g},${b})`;
 
 }
