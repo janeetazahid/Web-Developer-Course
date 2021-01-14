61415a1737bdb7848d156567b3b672266c237585
Janeeta Zahid <janeetazahid@gmail.com>
2021-01-13 21:50:11 -0500

    adding random color function

--- Events/eventsThis.js
+++ Events/eventsThis.js
@@ -1,3 +1,5 @@
+
+//function to create random color 
 const makeRandColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
@@ -5,18 +7,3 @@ const makeRandColor = () => {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-const buttons = document.querySelectorAll('button');
-
-for (let button of buttons) {
-    button.addEventListener('click', colorize)
-}
-
-const h1s = document.querySelectorAll('h1');
-for (let h1 of h1s) {
-    h1.addEventListener('click', colorize)
-}
-
-function colorize() {
-    this.style.backgroundColor = makeRandColor();
-    this.style.color = makeRandColor();
-}
