90fd96010dba10082f565021e3e8237f2750fb06
Janeeta Zahid <janeetazahid@gmail.com>
2021-01-14 20:32:05 -0500

    updating listner

--- Events/keyboardEvents.js
+++ Events/keyboardEvents.js
@@ -3,3 +3,11 @@
 document.querySelector('button').addEventListener('click',function(){
     alert("CLICKED")
 })
+
+
+const input=document.querySelector('input')
+
+
+input.addEventListener('keydown',function(e){
+    console.log(e)
+})
