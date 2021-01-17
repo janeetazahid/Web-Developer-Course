ca81ccf206912371679e0639dbb16c2788c262da
Janeeta Zahid <janeetazahid@gmail.com>
2021-01-16 21:07:15 -0500

    create new function

--- Events/formEvents.js
+++ Events/formEvents.js
@@ -2,13 +2,28 @@
 
 
 const tweetForm=document.querySelector("#tweetForm")
-const list=document.querySelector("ul")
+const list=document.querySelector("#tweets")
 
 tweetForm.addEventListener('submit',function(e){
     //prevent default behaviour
     e.preventDefault();
+    //get username and tweet value 
     const usernameInput= tweetForm.elements.username.value;
     const tweetInput= tweetForm.elements.tweet.value;
+    //add tweet to ul
+    addTweet(usernameInput,tweetInput)
+
+})
+
+
+//function to add tweet
+const addTweet=(usernameInput,tweetInput)=>{
+    /**
+     * Creates a new tweet in the form: username - tweet 
+     * @param string usernameInput: Username value 
+     * @param string tweetInput: Tweet value 
+     */
+
     //create new li
     const newTweet=document.createElement('li');
     //create new bold tag
@@ -19,7 +34,6 @@ tweetForm.addEventListener('submit',function(e){
     newTweet.append(bTag)
     //add the tweet to the li in form - tweet
     newTweet.append(` - ${tweetInput}`)
+    //add new tweet to ul 
     list.append(newTweet)
-
-
-})
+}
