bfea137d29fae91c2af2beaca59539dab5a6d918
Janeeta Zahid <janeetazahid@gmail.com>
2021-01-16 20:49:40 -0500

    prevent default behaviour

--- Events/formEvents.js
+++ Events/formEvents.js
@@ -5,4 +5,6 @@ const tweetForm=document.querySelector("#tweetForm")
 
 tweetForm.addEventListener('submit',function(e){
     alert("SUBMITTED");
+    //prevent default behaviour
+    e.preventDefault();
 })
