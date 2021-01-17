cbcd3dd1557cea334d0bb80440641495115d4482
Janeeta Zahid <janeetazahid@gmail.com>
2021-01-16 20:56:29 -0500

    update

--- Events/formEvents.js
+++ Events/formEvents.js
@@ -2,9 +2,15 @@
 
 
 const tweetForm=document.querySelector("#tweetForm")
+const list=document.querySelector("ul")
 
 tweetForm.addEventListener('submit',function(e){
-    alert("SUBMITTED");
+    const usernameInput= tweetForm.elements.username.value;
+    const tweetInput= tweetForm.elements.tweet.value;
     //prevent default behaviour
+    console.log(usernameInput);
+    console.log(tweetInput)
     e.preventDefault();
+  
+
 })
