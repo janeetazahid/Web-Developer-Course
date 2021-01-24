e60fa750a7358a4becf9ee45a7c2f83b354d07f6
Janeeta Zahid <janeetazahid@gmail.com>
2021-01-23 21:13:31 -0500

    updating js

--- Events/Score_Keeper/score_keeper.js
+++ Events/Score_Keeper/score_keeper.js
@@ -12,13 +12,19 @@ let player2Score=0; //start at 0 for both players
 
 let winningScore=5; //score needed to win 
 
+let gameOver=false; //wether the game is over or not
+
 //add listner event to buttons and listen for a click event 
 player1Button.addEventListener('click',function(){
-    if (player1Score!==winningScore)
+    if (!gameOver)
         {
             //increase score 
             player1Score+=1;
             //update display 
+            if (player1Score===winningScore)
+                {
+                    gameOver=true;
+                }
             player1Display.textContent=player1Score;
         }
 
@@ -26,11 +32,15 @@ player1Button.addEventListener('click',function(){
 
 
 player2Button.addEventListener('click',function(){
-    if (player2Score!==winningScore)
+    if (!gameOver)
         {
             //increase score 
             player2Score+=1;
             //update display 
+            if (player2Score===winningScore)
+                {
+                    gameOver=true;
+                }
             player2Display.textContent=player2Score;
         }
 
